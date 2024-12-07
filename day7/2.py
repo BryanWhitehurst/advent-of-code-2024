dc5d57bb19279254bb72901ec4252cc2ab7bbcc5
BryanWhitehurst <bwhitehurst4@gmail.com>
2024-12-06 22:50:53 -0800

    day7

---
+++ day7/2.py
@@ -0,0 +1,34 @@
+from itertools import product
+
+lines = []
+with open("input.txt", 'r') as current_file:
+    for line in current_file.readlines():
+        leftSide, rightSide = line.split(":")
+        leftSide = [int(leftSide)]
+        rightSide = rightSide.strip().split()
+        rightSide  = [ int(x) for x in rightSide ]
+        
+        lines.append(leftSide + rightSide)
+
+total = 0
+for line in lines: 
+    numOfOperators = len(line) - 2
+    operatorCombos = [''.join(x) for x in product(['+', '*', '|'], repeat = numOfOperators)]
+    operatorCombos = [list(x) for x in operatorCombos]
+
+    for combo in operatorCombos:
+        result = line[0]
+        curTotal = line[1]
+        for i in range(2, len(line)):
+            op = combo.pop(0)
+            if op == '+':
+                curTotal += line[i]
+            elif op == '*': 
+                curTotal *= line[i]
+            elif op == '|':
+                curTotal = int(str(curTotal) + str(line[i]))
+        if result == curTotal: 
+            total += result
+            break
+
+print(total)
